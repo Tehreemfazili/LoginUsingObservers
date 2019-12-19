339dd53b63359b82dfccad8ec665b8822bd2d8f9
Tehreem <tehreemfazili5@gmail.com>
2019-12-19 18:15:15 +0530

    added textview

--- app/src/main/java/com/example/admin/loginusingobservers/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/admin/loginusingobservers/viewModels/MainViewModel.kt
@@ -1,3 +1,9 @@
 package com.example.admin.loginusingobservers.viewModels
 
-class MainViewModel
+import android.arch.lifecycle.ViewModel
+import java.util.*
+
+class MainViewModel : ViewModel(){
+
+
+}
